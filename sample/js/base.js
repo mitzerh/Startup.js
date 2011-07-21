b28e4f4f01af4706be796aec95161f666b276741
Helcon Mabesa <hbmmitzer-git@yahoo.com>
2011-07-21 13:15:09 -0400

    Updated sample to reflect documentation

--- sample/js/base.js
+++ sample/js/base.js
@@ -1,38 +1,124 @@
-(function(){
-	// site
+(function($tartup){
+	
+	/** 
+	 * The 3 Important variables to declare
+	 */
 	var Add = $tartup.site.Add,
 		DOC_Ready = $tartup.site.OnDocReady,
 		PAGE_Ready = $tartup.site.OnPageReady;
 	
-	DOC_Ready("test",function() {
-		console.log("test");
+	
+	/*** Add Method ***/
+	
+	// This function will be accessible by calling:
+	// $tartup.site.SomeFunction();
+	Add("SomeFunction",function(user){
+	   log("Hello " + user + "!");
 	});
 	
-	DOC_Ready("test2",function(){
-		console.log("hello");
+	
+	// This object will be accessible:
+	// $tartup.site.SomeObject;
+	Add("SomeObject",{
+	   sayHello: function(user){
+		  user = user || "nobody";
+	      log("Hello " + user + "!");
+	   },
+	   sayGoodbye: function(user){
+		  user = user || "nobody";
+	      log("Goodbye " + user + "!");
+	   }
 	});
 	
-	DOC_Ready("channel.test3",function(){
-		console.log("test3 here");
+	/*** DOC_Ready Method ***/
+	
+	// Executing an anonymous function
+	// No reference is created, it is an anonymous function
+	DOC_Ready(function(){
+	   log("DOM is Ready on anonymous function!");
+	});
+ 
+
+	// This function will also be accessible by calling:
+	// $tartup.site.isDOMReady();
+	DOC_Ready("isDOMReady",function(){
+	   log("DOM is Ready on function!!!");
+	});
+ 
+	
+	// Executing an object
+	// This object will also be accessible:
+	// $tartup.site.ReadyObject;
+	DOC_Ready("ReadyObject",{
+	   // init function will triggered automatically
+	   // if it is not present, there is no execution done, but the object will still be created
+	   init: function() { 
+	      this.sayDomReady();
+	   },
+	   sayDomReady: function() {
+	      log("DOM is Ready on object!!!");
+	   }
+	});
+ 
+	
+	/*** PAGE_Ready ***/
+	
+	// Executing an anonymous function
+	// No reference is created, it is an anonymous function
+	PAGE_Ready(function(){
+	   log("Anonymous Page Ready!");
+	
+		// call custom object namespace
+		$tartup.site.myNamespace.sayHello("Mr. User");
 	});
 	
-	DOC_Ready("obj1",{
-		init: function() {
-			console.log("init 1");
+	
+	// This function will also be accessible by calling:
+	// $tartup.site.helloThere();
+	PAGE_Ready("helloThere",function(){
+	   log("Hello There Page Ready!!!");
+	});
+	
+	// Executing an object
+	// This object will also be accessible:
+	// $tartup.site.PageReadyObject;
+	PAGE_Ready("PageReadyObject",{
+	   // init function will triggered automatically
+	   // if it is not present, there is no execution done, but the object will still be created
+	   init: function() { 
+	      this.sayPageIsReady();
+	   },
+	   sayPageIsReady: function() {
+	      log("Page is Ready!!!");
+	   }
+	});
+	
+	/*** Custom Object Namespace ***/
+	
+	// This function will be accessible by calling:
+	// $tartup.site.myNamespace.hello();
+	Add("myNamespace.sayHello",function(user){
+	   user = user || "nobody";
+	   log("hello! " + user);
+	});
+
+	// This object will be accessible:
+	// $tartup.site.myNamespace;
+	Add("myNamespace", {
+	   goodBye: function() {
+	      log("goodbye son!");
+	   },
+	   farewell: function() {
+	      log("farewell son!");	
+	   }
+	});
+	
+	function log(str) {
+		if (window.console && console.log) {
+			console.log(str);
+		} else {
+			alert(str);
 		}
-	});
+	}
 	
-	DOC_Ready("obj2",{
-		init: function() {
-			console.log("init 2");
-		}
-	});
-	
-	PAGE_Ready("channel.obj2",{
-		init: function() {
-			console.log("page ready - ch init 2");
-		}
-	});
-	console.log($tartup)
-	
-}());
+}($tartup));
